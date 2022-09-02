478e143d804abf00d5c7989ad05299e11b2d578e
shakhidevvran <nurieva005@gmail.com>
2022-09-02 13:31:36 +0600

    homework3

--- clients/models.py
+++ clients/models.py
@@ -4,5 +4,10 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
-    active = models.BooleanField(default=False)
-    bottle_ordered = models.IntegerField(default=20)
+    active = models.BooleanField(default=True, verbose_name='является активным покупателем')
+    bottle_ordered = models.IntegerField(default=1)
+    photo = models.ImageField(verbose_name='Фото',
+                              upload_to='photos',
+                              null=True,
+                              blank=True
+                              )
