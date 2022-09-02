48cc0a9ac6c654a640a99ef01c31d1c006a06de2
shakhidevvran <nurieva005@gmail.com>
2022-09-02 18:16:50 +0600

    hw4

--- clients/models.py
+++ clients/models.py
@@ -11,3 +11,22 @@ class Client(models.Model):
                               null=True,
                               blank=True
                               )
+
+class Order(models.Model):
+    created_at = models.DateTimeField(verbose_name='дата и время создания заказа',
+                                      auto_now_add=True)
+    update_at = models.DateTimeField(
+        verbose_name='дата и время изменения заказа',
+        auto_now=True
+    )
+    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=255)
+    contacts = models.CharField(max_length=255)
+
+
+    def __str__(self):
+        return f'{self.name} - {self.contacts}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
